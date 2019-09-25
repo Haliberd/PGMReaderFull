acaed1bfa85d92885fe45f03b871d2ad38453690
Gerardo <gerardoestrada14@hotmail.com>
2019-09-25 13:17:26 -0300

    Added timers for each process

--- PGMReaderFull/src/pgmreaderfull/Paralelo.java
+++ PGMReaderFull/src/pgmreaderfull/Paralelo.java
@@ -34,6 +34,7 @@ public class Paralelo {
         if(opcion != 9){
             if(opcion < 6){
                 MonitorDeImagen monitor = new MonitorDeImagen(matrizO, picWidth, picHeight, maxvalue);
+                double startTimeD = System.currentTimeMillis();
 
                 Thread[] thr = new Thread[(picHeight/2)];
                 for(int i = 0; i < (picHeight/2); i++){
@@ -57,9 +58,13 @@ public class Paralelo {
                 SalidaArchivo sa = new SalidaArchivo(picWidth, picHeight, maxvalue, "imagen"+opcion+"D");
 
                 sa.EscribirAArchivo(imagenN);
+                double endTimeD = System.currentTimeMillis() - startTimeD;
+                double secondsD = endTimeD/1000.0; 
+                System.out.print("Duracion dilatacion (segundos): "+ secondsD+"\n");
 
 
                 monitor.matrizO = lectorI.devolverImagen();
+                double startTimeE = System.currentTimeMillis();
                 thr = new Thread[(picHeight/2)];
                 for(int i = 0; i < (picHeight/2); i++){
                     thr[i] = new Thread( new EscritorFila(2*i, monitor, opcion+5) );
@@ -80,9 +85,15 @@ public class Paralelo {
                 sa = new SalidaArchivo(picWidth, picHeight, maxvalue, "imagen"+opcion+"E");
 
                 sa.EscribirAArchivo(imagenN);
+            
+                double endTimeE = System.currentTimeMillis() - startTimeE;
+                double secondsE = endTimeE/1000.0; 
+                System.out.print("Duracion erosion (segundos): "+ secondsE+"\n");
+                
             }
             else if(opcion == 6){
                 MonitorDeImagen monitor = new MonitorDeImagen(matrizO, picWidth, picHeight, maxvalue);
+                double startTime6 = System.currentTimeMillis();
 
                 Thread[] thr = new Thread[(picHeight/2)];
                 for(int i = 0; i < (picHeight/2); i++){
@@ -119,10 +130,14 @@ public class Paralelo {
                 SalidaArchivo sa = new SalidaArchivo(picWidth, picHeight, maxvalue, "imagenDE");
 
                 sa.EscribirAArchivo(imagenN);
+                double endTime6 = System.currentTimeMillis() - startTime6;
+                double seconds6 = endTime6/1000.0; 
+                System.out.print("Duracion ejecucion (segundos): "+ seconds6+"\n");
             }
             else if(opcion == 7){
                 MonitorDeImagen monitor = new MonitorDeImagen(matrizO, picWidth, picHeight, maxvalue);
 
+                double startTime7 = System.currentTimeMillis();
                 Thread[] thr = new Thread[(picHeight/2)];
 
                 thr = new Thread[(picHeight/2)];
@@ -160,6 +175,9 @@ public class Paralelo {
                 SalidaArchivo sa = new SalidaArchivo(picWidth, picHeight, maxvalue, "imagenED");
 
                 sa.EscribirAArchivo(imagenN);
+                double endTime7 = System.currentTimeMillis() - startTime7;
+                double seconds7 = endTime7/1000.0; 
+                System.out.print("Duracion ejecucion (segundos): "+ seconds7+"\n");
             }
         
         }
--- PGMReaderSecuencial/src/pgmreadersecuencial/Secuencial.java
+++ PGMReaderSecuencial/src/pgmreadersecuencial/Secuencial.java
@@ -33,6 +33,7 @@ public class Secuencial {
         switch(opcion){
             case 1:
                 this.matrizO = lectorI.devolverImagen();
+                double startTime31D = System.currentTimeMillis();
                 for(int i = 0; i<(picHeight/2); i++){
                     escribir3_1D(2*i);
                     //System.out.println("F");
@@ -42,7 +43,11 @@ public class Secuencial {
                     //System.out.println("E");
                 }
                 new SalidaArchivo(picWidth, picHeight, maxvalue, "imagenOpcion"+opcion+"D").EscribirAArchivo(matrizO);
+                double endTime31D = System.currentTimeMillis() - startTime31D;
+                double seconds31D = endTime31D/1000.0; 
+                System.out.print("Duracion dilatacion (segundos): "+ seconds31D+"\n");
                 this.matrizO = lectorI.devolverImagen();
+                double startTime31E = System.currentTimeMillis();
                 for(int i = 0; i<(picHeight/2); i++){
                     escribir3_1E(2*i);
                     //System.out.println("A");
@@ -52,9 +57,13 @@ public class Secuencial {
                     //System.out.println("R");
                 }
                 new SalidaArchivo(picWidth, picHeight, maxvalue, "imagenOpcion"+opcion+"E").EscribirAArchivo(matrizO);
+                double endTime31E = System.currentTimeMillis() - startTime31E;
+                double seconds31E = endTime31E/1000.0; 
+                System.out.print("Duracion erosion (segundos): "+ seconds31E+"\n");
                 break;
             case 2:
                 this.matrizO = lectorI.devolverImagen();
+                double startTime321D = System.currentTimeMillis();
                 for(int i = 0; i<(picHeight/2); i++){
                     escribir3_2_1D(2*i);
                     //System.out.println("F");
@@ -64,7 +73,12 @@ public class Secuencial {
                     //System.out.println("E");
                 }
                 new SalidaArchivo(picWidth, picHeight, maxvalue, "imagenOpcion"+opcion+"D").EscribirAArchivo(matrizO);
+                double endTime321D = System.currentTimeMillis() - startTime321D;
+                double seconds321D = endTime321D/1000.0; 
+                System.out.print("Duracion dilatacion (segundos): "+ seconds321D+"\n");
+                
                 this.matrizO = lectorI.devolverImagen();
+                double startTime321E = System.currentTimeMillis();
                 for(int i = 0; i<(picHeight/2); i++){
                     escribir3_2_1E(2*i);
                     //System.out.println("A");
@@ -74,9 +88,13 @@ public class Secuencial {
                     //System.out.println("R");
                 }
                 new SalidaArchivo(picWidth, picHeight, maxvalue, "imagenOpcion"+opcion+"E").EscribirAArchivo(matrizO);
+                double endTime321E = System.currentTimeMillis() - startTime321E;
+                double seconds321E = endTime321E/1000.0; 
+                System.out.print("Duracion erosion (segundos): "+ seconds321E+"\n");
                 break;
             case 3:
                 this.matrizO = lectorI.devolverImagen();
+                double startTime031D = System.currentTimeMillis();
                 for(int i = 0; i<(picHeight/2); i++){
                     escribir0_3_1D(2*i);
                     //System.out.println("F");
@@ -86,7 +104,11 @@ public class Secuencial {
                     //System.out.println("E");
                 }
                 new SalidaArchivo(picWidth, picHeight, maxvalue, "imagenOpcion"+opcion+"D").EscribirAArchivo(matrizO);
+                double endTime031D = System.currentTimeMillis() - startTime031D;
+                double seconds031D = endTime031D/1000.0; 
+                System.out.print("Duracion dilatacion (segundos): "+ seconds031D+"\n");
                 this.matrizO = lectorI.devolverImagen();
+                double startTime031E = System.currentTimeMillis();
                 for(int i = 0; i<(picHeight/2); i++){
                     escribir0_3_1E(2*i);
                     //System.out.println("A");
@@ -96,9 +118,13 @@ public class Secuencial {
                     //System.out.println("R");
                 }
                 new SalidaArchivo(picWidth, picHeight, maxvalue, "imagenOpcion"+opcion+"E").EscribirAArchivo(matrizO);
+                double endTime031E = System.currentTimeMillis() - startTime031E;
+                double seconds031E = endTime031E/1000.0; 
+                System.out.print("Duracion erosion (segundos): "+ seconds031E+"\n");
                 break;
             case 4:
                 this.matrizO = lectorI.devolverImagen();
+                double startTime032D = System.currentTimeMillis();
                 for(int i = 0; i<(picHeight/2); i++){
                     escribir0_3_2D(2*i);
                     //System.out.println("F");
@@ -108,7 +134,11 @@ public class Secuencial {
                     //System.out.println("E");
                 }
                 new SalidaArchivo(picWidth, picHeight, maxvalue, "imagenOpcion"+opcion+"D").EscribirAArchivo(matrizO);
+                double endTime032D = System.currentTimeMillis() - startTime032D;
+                double seconds032D = endTime032D/1000.0; 
+                System.out.print("Duracion dilatacion (segundos): "+ seconds032D+"\n");
                 this.matrizO = lectorI.devolverImagen();
+                double startTime032E = System.currentTimeMillis();
                 for(int i = 0; i<(picHeight/2); i++){
                     escribir0_3_2E(2*i);
                     //System.out.println("A");
@@ -118,9 +148,13 @@ public class Secuencial {
                     //System.out.println("R");
                 }
                 new SalidaArchivo(picWidth, picHeight, maxvalue, "imagenOpcion"+opcion+"E").EscribirAArchivo(matrizO);
+                double endTime032E = System.currentTimeMillis() - startTime032E;
+                double seconds032E = endTime032E/1000.0; 
+                System.out.print("Duracion erosion (segundos): "+ seconds032E+"\n");
                 break;
             case 5:
                 this.matrizO = lectorI.devolverImagen();
+                double startTime0232D = System.currentTimeMillis();
                 for(int i = 0; i<(picHeight/2); i++){
                     escribir0_2_3_2_0D(2*i);
                     //System.out.println("F");
@@ -130,7 +164,11 @@ public class Secuencial {
                     //System.out.println("E");
                 }
                 new SalidaArchivo(picWidth, picHeight, maxvalue, "imagenOpcion"+opcion+"D").EscribirAArchivo(matrizO);
+                double endTime0232D = System.currentTimeMillis() - startTime0232D;
+                double seconds0232D = endTime0232D/1000.0; 
+                System.out.print("Duracion dilatacion (segundos): "+ seconds0232D+"\n");
                 this.matrizO = lectorI.devolverImagen();
+                double startTime0232E = System.currentTimeMillis();
                 for(int i = 0; i<(picHeight/2); i++){
                     escribir0_2_3_2_0E(2*i);
                     //System.out.println("A");
@@ -140,9 +178,13 @@ public class Secuencial {
                     //System.out.println("R");
                 }
                 new SalidaArchivo(picWidth, picHeight, maxvalue, "imagenOpcion"+opcion+"E").EscribirAArchivo(matrizO);
+                double endTime0232E = System.currentTimeMillis() - startTime0232E;
+                double seconds0232E = endTime0232E/1000.0; 
+                System.out.print("Duracion erosion (segundos): "+ seconds0232E+"\n");
                 break;
             case 6:
                 this.matrizO = lectorI.devolverImagen();
+                double startTime031DE = System.currentTimeMillis();
                 for(int i = 0; i<(picHeight/2); i++){
                     escribir0_3_1D(2*i);
                     //System.out.println("F");
@@ -160,8 +202,12 @@ public class Secuencial {
                     //System.out.println("R");
                 }
                 new SalidaArchivo(picWidth, picHeight, maxvalue, "imagenDE").EscribirAArchivo(matrizO);
+                double endTime031DE = System.currentTimeMillis() - startTime031DE;
+                double seconds031DE = endTime031DE/1000.0; 
+                System.out.print("Duracion erosion (segundos): "+ seconds031DE+"\n");
                 break;
             case 7:
+                double startTime031ED = System.currentTimeMillis();
                 this.matrizO = lectorI.devolverImagen();
                 for(int i = 0; i<(picHeight/2); i++){
                     escribir0_3_1E(2*i);
@@ -181,6 +227,9 @@ public class Secuencial {
                 }
                 
                 new SalidaArchivo(picWidth, picHeight, maxvalue, "imagenED").EscribirAArchivo(matrizO);
+                double endTime031ED = System.currentTimeMillis() - startTime031ED;
+                double seconds031ED = endTime031ED/1000.0; 
+                System.out.print("Duracion erosion (segundos): "+ seconds031ED+"\n");
                 break;
         }
     }
